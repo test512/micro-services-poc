2f57e45b33fa0bb3c0489dda8a89560119a58b5a
Prateek Ashtikar <prateek.ashtikar@rediffmail.com>
2018-04-11 17:36:46 +0530

    Spring Boot Admin - Admin UI For Monitoring Microservices

---
+++ Spring Boot Admin - Admin UI For Monitoring Microservices/employee-consumer/src/main/java/com/javainuse/controllers/ConsumerControllerClient.java
@@ -0,0 +1,34 @@
+package com.javainuse.controllers;
+
+import java.io.IOException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+public class ConsumerControllerClient {
+
+	public void getEmployee() throws RestClientException, IOException {
+
+		String baseUrl = "http://localhost:8080/employee";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response=null;
+		try{
+		response=restTemplate.exchange(baseUrl,
+				HttpMethod.GET, getHeaders(),String.class);
+		}catch (Exception ex)
+		{
+			System.out.println(ex);
+		}
+		System.out.println(response.getBody());
+	}
+
+	private static HttpEntity<?> getHeaders() throws IOException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		return new HttpEntity<>(headers);
+	}
+}
